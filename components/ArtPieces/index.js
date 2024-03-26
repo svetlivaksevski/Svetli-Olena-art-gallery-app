320cefa8cc15b0ec00cf81d03802ec98ece14326
OLENA DOBREVA <sectionone@ukr.net>
2024-03-26 16:42:26 +0100

    styling palette in circles

--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import ArtPiecePreview from "../ArtPiecesPreview/index.js";
+import styled from "styled-components";
+
+const DivArtPieces = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: auto;
+  gap: 20px;
+`;
 
 export default function ArtPieces({ pieces, onToggleFavorite, artPiecesInfo }) {
   return (
     <>
       <h2>Art Pieces</h2>
-      {pieces.map((piece) => (
-        <ArtPiecePreview
-          key={piece.slug}
-          {...piece}
-          onToggleFavorite={onToggleFavorite}
-          isFavorite={
-            artPiecesInfo.find((info) => info.slug === piece.slug)
-              ?.isFavorite || false
-          }
-        />
-      ))}
+      <DivArtPieces>
+        {pieces.map((piece) => (
+          <ArtPiecePreview
+            key={piece.slug}
+            {...piece}
+            onToggleFavorite={onToggleFavorite}
+            isFavorite={
+              artPiecesInfo.find((info) => info.slug === piece.slug)
+                ?.isFavorite || false
+            }
+          />
+        ))}
+      </DivArtPieces>
     </>
   );
 }

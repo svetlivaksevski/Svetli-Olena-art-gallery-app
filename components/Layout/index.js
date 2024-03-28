dd2d97c95b802f6707f785080c43332cdbde4c46
svetlivaksevski <svetlivaksevski@gmail.com>
2024-03-28 09:44:16 +0100

    Add styling

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,18 +1,10 @@
-import Link from "next/link";
 import Navigation from "../Navigation";
-import styled from "styled-components";
 
 export default function Layout({ children }) {
   return (
-    <div
-      style={{
-        backgroundImage: `url("/background2.jpg")`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <>
       {children}
       <Navigation />
-    </div>
+    </>
   );
 }
